7aede31e866419423a95d8450ee7561f0bd49719
WijdaneCHAR <87330491+WijdaneCHAR@users.noreply.github.com>
2022-04-08 15:22:28 +0000

    build in android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,6 +5,17 @@ const config: CapacitorConfig = {
   appName: 'myLoginProject',
   webDir: 'www',
   bundledWebRuntime: false,
+  cordova: {
+    preferences: {
+      ScrollEnabled: 'false',
+      BackupWebStorage: 'none',
+      SplashMaintainAspectRatio: 'true',
+      FadeSplashScreenDuration: '300',
+      SplashShowOnlyFirstTime: 'false',
+      SplashScreen: 'screen',
+      SplashScreenDelay: '3000'
+    }
+  }
 };
 
 export default config;
